a7f585b4a0f9e43f41cbc262ca2d0acbc00c914f
Assasans <swimmin2@gmail.com>
2019-04-30 13:22:22 +0300

    Update logger

--- utils.js
+++ utils.js
@@ -1,8 +1,20 @@
+const app = require('./app');
+
 const sprintf = require('sprintf-js').sprintf;
 const chalk = require('chalk');
+const Enum = require('enum');
 
 module.exports.Utils = function() {
-	this.log = function(message, module, show_time) {
+	this.Level = Object.freeze(new Enum({
+		DISABLE: -1,
+		DEBUG: 1,
+		INFO: 2,
+		WARN: 3,
+		ERROR: 4,
+		FATAL: 5
+	}));
+
+	this.log = function(message, module, level, show_time) {
 		var result = String();
 
 		if((show_time === undefined) || (show_time === true)) {
@@ -13,10 +25,32 @@ module.exports.Utils = function() {
 		}
 
 		if(module === undefined) {
-			result += chalk.keyword('lime').bold("[Application]: ");
+			result += chalk.keyword('lime').bold("[Application]");
 		} else {
-			result += chalk.keyword('lime').bold(sprintf("[%s]: ", module));
+			result += chalk.keyword('lime').bold(sprintf("[%s]", module));
 		}
+
+		switch(level) {
+			case this.Level.DEBUG:
+				result += chalk.keyword('violet').bold(sprintf(" [%s]", "DEBUG"));
+				break;
+			case this.Level.INFO:
+				result += chalk.keyword('lime').bold(sprintf(" [%s]", "INFO"));
+				break;
+			case this.Level.WARN:
+				result += chalk.keyword('orange').bold(sprintf(" [%s]", "WARN"));
+				break;
+			case this.Level.ERROR:
+				result += chalk.keyword('red').bold(sprintf(" [%s]", "ERROR"));
+				break;
+			case this.Level.FATAL:
+				//Dark red color
+				result += chalk.hex(0x8B0000).bold(sprintf(" [%s]", "FATAL"));
+				break;
+			default:
+				break;
+		}
+		result += chalk.keyword('lime').bold(": ");
 
 		result += message;
 
