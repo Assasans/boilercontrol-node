4cb935dc60aa20ba4fe70bd59f5651ad149aa068
Assasans <swimmin2@gmail.com>
2019-04-30 15:52:58 +0300

    Debug changes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express'
+router.get('/', function(request, resopnse, next) {
+  resopnse.render('index', {
+    logged: Math.random() >= 0.5
   });
 });
 
